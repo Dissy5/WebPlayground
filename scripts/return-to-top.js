32d268f5c8723934d705915b635407daa2e5fe1d
Dissy5 <42944940+Dissy5@users.noreply.github.com>
2026-08-21 21:53:58 -0700

    return to top button added to element reference page.
    boilerplate improved for individual element reference pages

---
+++ scripts/return-to-top.js
@@ -0,0 +1,13 @@
+const returnToTopButton = document.getElementById("return-to-top");
+
+function toggleReturnToTopButton() {
+  if (window.scrollY > window.innerHeight / 2) {
+    returnToTopButton.style.display = "block";
+  } else {
+    returnToTopButton.style.display = "none";
+  }
+}
+
+// Listen for scroll events
+
+window.addEventListener("scroll", toggleReturnToTopButton);
